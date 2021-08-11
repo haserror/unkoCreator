d8662fe2fd1c7424db4693a334bdb6b833a24ec7
haserror <c.c.junkie2@gmail.com>
2021-08-11 16:34:47 +0900

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAkita = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(930, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnAkita);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 484);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(930, 71);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.btnDecrypt);
+            this.panel3.Controls.Add(this.btnEncrypt);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 48);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(930, 436);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(381, 401);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(410, 81);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(103, 45);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "○○化→";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.BtnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(410, 273);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(103, 45);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "←もどし";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.BtnDecrypt_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(535, 6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(381, 401);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnAkita
+            // 
+            this.btnAkita.Location = new System.Drawing.Point(815, 14);
+            this.btnAkita.Name = "btnAkita";
+            this.btnAkita.Size = new System.Drawing.Size(103, 45);
+            this.btnAkita.TabIndex = 4;
+            this.btnAkita.Text = "飽きた";
+            this.btnAkita.UseVisualStyleBackColor = true;
+            this.btnAkita.Click += new System.EventHandler(this.BtnAkita_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(89, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 555);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Sys";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAkita;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string WORD0 = "う";
+        private const string WORD1 = "ん";
+        private const string WORD2 = "こ";
+        private const string WORD3 = "ー";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitle.Text = "うんこ製造機";
+            btnEncrypt.Text = "うんこ→";
+            textBox1.Text = "うんこにしたい文字列を入力してね";
+        }
+
+        private void BtnEncrypt_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = encrypt(textBox1.Text);
+        }
+
+        private void BtnDecrypt_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = decrypt(textBox2.Text);
+        }
+
+        private void BtnAkita_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private string encrypt(string s)
+        {
+            string rtn = "";
+
+            foreach(char c in s.ToCharArray())
+            {
+                int code = (int)c;
+                string encoded = RadixConvert.ToString(code, 4, true);
+                for(int l = encoded.Length; l < 8; l++)
+                {
+                    encoded = "0" + encoded;
+                }
+
+                rtn += encoded;
+            }
+
+            rtn = rtn.Replace("0", WORD0);
+            rtn = rtn.Replace("1", WORD1);
+            rtn = rtn.Replace("2", WORD2);
+            rtn = rtn.Replace("3", WORD3);
+
+            return rtn;
+        }
+
+        private string decrypt(string s)
+        {
+            string rtn = "";
+
+            for (int i = 0; i < s.Length; i += 8)
+            {
+                string val = s.Substring(i, 8);
+                val = val.Replace(WORD0, "0");
+                val = val.Replace(WORD1, "1");
+                val = val.Replace(WORD2, "2");
+                val = val.Replace(WORD3, "3");
+
+                int code = (int)RadixConvert.ToDecimal(val, 4);
+                rtn += (char)code;
+            }
+
+            return rtn;
+        }
+
+    }
+}
